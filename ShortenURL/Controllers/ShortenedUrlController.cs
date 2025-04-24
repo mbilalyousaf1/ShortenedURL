86e226d2045e47baaaaa99c02b65af68dd7829d8
Bilal Yousaf <Bilal Yousaf@DESKTOP-06RJ6TE>
2025-04-24 03:12:26 -0700

    Add project files.

---
+++ ShortenURL/Controllers/ShortenedUrlController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using ShortenURL.Entities;
+
+namespace ShortenURL.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShortenedUrlController : ControllerBase
+    {
+
+       
+     
+        
+
+
+    }
+}
+
+        
+
+    
+
+
---
+++ ShortenURL/Entities/ShortenedUrl.cs
@@ -0,0 +1,15 @@
+namespace ShortenURL.Entities
+{
+    public class ShortenUrl
+    {
+        public Guid Id { get; set; }
+
+        public string LongUrl { get; set; } = string.Empty;
+
+        public string ShortUrl { get; set; } = string.Empty;
+
+        public DateTime CreatedOnUtc { get; set; }
+        prop
+
+    }
+}
